a84cc4088cb3642951f42f71150877aaa826ba63
jfhr <me@jfhr.de>
2021-11-14 10:13:23 +0100

    add tests for delta-to-intermediate
    - adjacent code blocks should be merged into one

---
+++ src/delta-to-intermediate.spec.js
@@ -0,0 +1,97 @@
+const test = require("ava");
+const deltaToIntermediate = require("./delta-to-intermediate");
+
+
+function deltaToIntermediateNormalized(delta) {
+    const intermediate = deltaToIntermediate(delta);
+    return JSON.parse(JSON.stringify(intermediate));
+}
+
+
+test('plain text', t => {
+    const expected = [{
+        attributes: {},
+        children: [{
+            attributes: {},
+            insert: 'Hello, world!',
+        }],
+    }];
+    const actual = deltaToIntermediateNormalized({
+        ops: [
+            {insert: 'Hello, world!\n'}
+        ]
+    });
+    t.deepEqual(actual, expected);
+});
+
+test('code block', t => {
+    const expected = [
+        {
+            attributes: {
+                'code-block': true,
+            },
+            children: [
+                {
+                    attributes: {},
+                    insert: 'let a;',
+                },
+                {
+                    attributes: {},
+                    insert: 'let b;',
+                },
+            ],
+        },
+        {
+            attributes: {},
+            children: [],
+        },
+    ];
+    const actual = deltaToIntermediateNormalized({
+        ops: [
+            {insert: 'let a;'},
+            {insert: '\n', attributes: {'code-block': true}},
+            {insert: 'let b;'},
+            {insert: '\n', attributes: {'code-block': true}},
+        ]
+    });
+    t.deepEqual(actual, expected);
+});
+
+test('code block and text', t => {
+    const expected = [
+        {
+            attributes: {
+                'code-block': true,
+            },
+            children: [
+                {
+                    attributes: {},
+                    insert: 'let a;',
+                },
+                {
+                    attributes: {},
+                    insert: 'let b;',
+                },
+            ],
+        },
+        {
+            attributes: {},
+            children: [
+                {
+                    attributes: {},
+                    insert: 'plain text',
+                }
+            ],
+        },
+    ];
+    const actual = deltaToIntermediateNormalized({
+        ops: [
+            {insert: 'let a;'},
+            {insert: '\n', attributes: {'code-block': true}},
+            {insert: 'let b;'},
+            {insert: '\n', attributes: {'code-block': true}},
+            {insert: 'plain text\n'},
+        ]
+    });
+    t.deepEqual(actual, expected);
+});
